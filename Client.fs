4fe7613cfb59d41de6c69d5afef24e3ceed2159d
Mikhail Chernienko <mike.chernienko@sharp-dev.net>
2017-09-01 10:53:27 +1000

    Migtayed to .NET Framework 4.5.2; namespace added

--- Client.fs
+++ Client.fs
@@ -1,72 +1,74 @@
-module Client
+namespace ytClient
 
-open System.Text
-open System.Net
-open System.Net.Http
-open Settings
-open Newtonsoft.Json
-open DTO
+module Client =
 
-type RestClient () =
-    let _server = settings.ApiDestination
-    let _apikey = settings.ApiKey
+    open System.Text
+    open System.Net
+    open System.Net.Http
+    open Settings
+    open Newtonsoft.Json
+    open DTO
 
-    let interprete (response:HttpResponseMessage)=
-        (
-            response.StatusCode, 
-            if response.IsSuccessStatusCode then 
-               response.Content.ReadAsStringAsync().Result
+    type RestClient () =
+        let _server = settings.ApiDestination
+        let _apikey = settings.ApiKey
+
+        let interprete (response:HttpResponseMessage)=
+            (
+                response.StatusCode, 
+                if response.IsSuccessStatusCode then 
+                   response.Content.ReadAsStringAsync().Result
+                else
+                   response.ReasonPhrase
+            )
+
+        let flatten set =
+            if Seq.isEmpty set then
+                ""
             else
-               response.ReasonPhrase
-        )
+                set 
+                |> Seq.map (fun x -> "&"+x)
+                |> String.concat ""
 
-    let flatten set =
-        if Seq.isEmpty set then
-            ""
-        else
-            set 
-            |> Seq.map (fun x -> "&"+x)
-            |> String.concat ""
+        let makeUri route options =
+            _server + "/" + 
+            route + 
+            "?key=" + _apikey + 
+            flatten options
+            
+        let client = new HttpClient()
 
-    let makeUri route options =
-        _server + "/" + 
-        route + 
-        "?key=" + _apikey + 
-        flatten options
-        
-    let client = new HttpClient()
+        let Post route body =
+            client.PostAsync((route, []) ||> makeUri, new StringContent(body, Encoding.UTF8, "application/json")).Result
+            |> interprete
 
-    let Post route body =
-        client.PostAsync((route, []) ||> makeUri, new StringContent(body, Encoding.UTF8, "application/json")).Result
-        |> interprete
+        let Put route body =
+            client.PutAsync((route, []) ||> makeUri, new StringContent(body, Encoding.UTF8, "application/json")).Result
+            |> interprete
 
-    let Put route body =
-        client.PutAsync((route, []) ||> makeUri, new StringContent(body, Encoding.UTF8, "application/json")).Result
-        |> interprete
+        let Get route options =
+            client.GetAsync((route, options) ||> makeUri).Result
+            |> interprete
 
-    let Get route options =
-        client.GetAsync((route, options) ||> makeUri).Result
-        |> interprete
+        let Delete route =
+            client.DeleteAsync((route, []) ||> makeUri).Result
+            |> interprete
 
-    let Delete route =
-        client.DeleteAsync((route, []) ||> makeUri).Result
-        |> interprete
+        member this.RetrieveResult<'T> response =
+            if (fst response) <> HttpStatusCode.OK then
+                failwith (snd response)
+            else
+                (snd response)
+                |> JsonConvert.DeserializeObject<'T>
 
-    member this.RetrieveResult<'T> response =
-        if (fst response) <> HttpStatusCode.OK then
-            failwith (snd response)
-        else
-            (snd response)
-            |> JsonConvert.DeserializeObject<'T>
+        member this.GetLanguages() =
+            Get "getLangs" []
+            |> this.RetrieveResult<LangList>
 
-    member this.GetLanguages() =
-        Get "getLangs" []
-        |> this.RetrieveResult<LangList>
+        member this.Detect text =
+            Get "detect" ["text="+text]
+            |> this.RetrieveResult<LangDTO>
 
-    member this.Detect text =
-        Get "detect" ["text="+text]
-        |> this.RetrieveResult<LangDTO>
-
-    member this.Translate lang text =
-        Get "translate" ["text="+text; "lang="+lang]
-        |> this.RetrieveResult<Translation>
+        member this.Translate lang text =
+            Get "translate" ["text="+text; "lang="+lang]
+            |> this.RetrieveResult<Translation>
